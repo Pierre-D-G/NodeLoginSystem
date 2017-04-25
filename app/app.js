f8e9aecd7a47df7b25c73b56a1d7d8c63b328bbb
Pierre-D-G <Pierre-D-G@users.noreply.github.com>
2017-04-25 11:00:35 -0400

    app middleware
    Getting all the middleware needed setup

--- app/app.js
+++ app/app.js
@@ -1,10 +1,22 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var express = require('express'),
+    path = require('path'),
+    favicon = require('serve-favicon'),
+    logger = require('morgan'),
+    cookieParser = require('cookie-parser'),
+    session = require('express-session'),
+    passport = require('passport'),
+    localStrategy = require('passport-local')
+    bodyParser = require('body-parser'),
+    ExpressValidator = require('express-validator'),
+    multer = require('multer'),
+    flash = require('connect-flash'),
+    mongo = require('mongodb'),
+    mongoose = require('mongoose')
+    
 
+mongoose.connect("mongodb://localhost/nodeauth")
+
+// ROUTES
 var index = require('./routes/index');
 var users = require('./routes/users');
 
@@ -12,15 +24,59 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
+
+// Handle file uploads
+var upload = multer({
+  dest: './uploads'
+});
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+// Validator
+
+app.use(ExpressValidator({
+ errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Handle express sessions
+
+app.use(session({
+  secret: 'secret',
+  saveUninitialized: true,
+  resave: true
+}));
+
+// Passport
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Messages
+app.use(flash());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
 
 app.use('/', index);
 app.use('/users', users);
