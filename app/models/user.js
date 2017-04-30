f2c913c21d5729553cc7bd42b7b73df4138335e7
Pierre-D-G <gamil.pierre@gmail.com>
2017-04-30 12:52:06 -0400

    User model Tweaks

--- app/models/user.js
+++ app/models/user.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-var bcrypt = require('bcrypt-nodejs');
+var bcrypt = require('bcryptjs');
 var database = mongoose.connection;
 
 
@@ -9,52 +9,40 @@ mongoose.connect("mongodb://localhost/nodeauth");
 var userSchema = new mongoose.Schema({
     name: String,
     email: String,
-    username: String,
-    password: {
+    password: String,
+    username: {
         type: String,
-        required: true,
-        bcrypt: true
-    }
+        index: true
+    },
 });
 
 
-userSchema.pre('save', function (next) {
-    var user = this;
-    if (this.isModified('password') || this.isNew) {
-        bcrypt.genSalt(10, function (err, salt) {
-            if (err) {
-                return next(err);
-            }
-            bcrypt.hash(user.password, salt, null, function (err, hash) {
-                if (err) {
-                    return next(err);
-                }
-                user.password = hash;
-                next();
-            });
-        });
-    } else {
-        return next();
-    }
-});
 
-// Method for checking if password is valid by comparing it to the hashed password in the database
-userSchema.methods.validPassword = function (passw, cb) {
-    bcrypt.compareSync(passw, this.password, function (err, isMatch) {
-        if (err) {
-            return cb(err);
-        } else {
-            cd(null, isMatch);
-        }
+
+var User = module.exports = mongoose.model('User', userSchema);
+
+module.exports.getUserByUsername = function(username, cb){
+    query = {username: username};
+    User.findOne(query, cb)
+}
+
+
+module.exports.getUserById = function(id, cb){
+    User.findById(id, cb)
+}
+
+module.exports.createUser = function(newUser, callback){
+	bcrypt.genSalt(10, function(err, salt) {
+	    bcrypt.hash(newUser.password, salt, function(err, hash) {
+	        newUser.password = hash;
+	        newUser.save(callback);
+	    });
+	});
+}
+
+module.exports.validPassword = function (userPassword, hash, cb) {
+    bcrypt.compare(userPassword, hash, function (err, isMatch) {
+        if (err) throw err;
+        cb(null, isMatch);
     });
 };
-
-module.exports = mongoose.model('User', userSchema);
-
-// module.exports.createUser =  function(newUser, callback){
-//     bcrypt.hashSync(newUser.password, bcrypt.genSaltSync(10), function(err){
-//       if(err) throw err;
-//     newUser.password = newUser.generateHash(newUser.password);
-//     newUser.save(callback);
-//     });
-// };
