2bb0291f83d4ea230099cf4a9ffea401bab77c5d
Pierre-D-G <gamil.pierre@gmail.com>
2017-04-30 12:51:47 -0400

    Changed encryption system to bcryptjs since bcrypt-nodejs is deprecated

--- app/app.js
+++ app/app.js
@@ -70,7 +70,10 @@ app.use(passport.session());
 // Messages
 app.use(flash());
 app.use(function (req, res, next) {
-  res.locals.messages = require('express-messages')(req, res);
+res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  res.locals.user = req.user || null;
   next();
 });
 
