89ca9f4c8ea92883f05514e6a22871a28c115aae
Pierre-D-G <gamil.pierre@gmail.com>
2017-04-30 12:52:33 -0400

    Routing Tweaks

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
+function ensureAuthenticated(req, res, next){
+	if(req.isAuthenticated()){
+		return next();
+	}
+	res.redirect('/users/login');
+};
 module.exports = router;
