97be5ac9ba419181310aa40ea5c4702839be56a3
Pierre-D-G <gamil.pierre@gmail.com>
2017-04-30 16:05:21 -0400

    Access control

--- app/routes/users.js
+++ app/routes/users.js
@@ -107,6 +107,12 @@ router.post('/login',
     res.redirect('/');
   });
 
+// Logout
 
+router.get('/logout', function(req, res, next){
+  req.logout();
+  req.flash('success', 'You have logged out!');
+  res.redirect('/users/login');
+})
 
 module.exports = router;
